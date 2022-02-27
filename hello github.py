879a0fa88bbe73c3b39a7aee6f60d03d26aa9aff
bas4673 <bas4673@stacmail.net>
2022-02-28 12:37:31 +1300

    start the raffle
    error catching first input

--- hello github.py
+++ hello github.py
@@ -1,2 +1,10 @@
-# Write your code here :-)
-print("hello github")
+# input for prize in rafle
+get_prize = True
+while get_prize == True:
+    prize = input("Please enter a prize for the winner of the\n")
+    if prize == "":
+        print("please enter a prize")
+    else:
+        get_prize = False
+    prize_value = input(f"How much does the {prize} cost\n")
+    prize_value = int(prize_value)
