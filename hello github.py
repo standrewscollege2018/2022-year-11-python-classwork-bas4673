5f986a000e45355da88c1593dd0bd4fe773b888a
bas4673 <bas4673@stacmail.net>
2022-03-03 09:22:42 +1300

    finished up all error catching and create random.
    also finished final output.

--- hello github.py
+++ hello github.py
@@ -8,21 +8,36 @@ while get_prize == True:
         prize = input(f"'{prize}' is not a valid input, please enter a prize\n")
     else:
         get_prize = False
-#find out how much prize costs
-prize_value = input(f"How much does the {prize} cost\n")
-prize_value = int(prize_value)
+#find out how much prize costs + make sure value greater than 0
+get_number = True
+while get_number == True:
+    try:
+        prize_value = input(f"How much does the {prize} cost\n")
+        prize_value = int(prize_value)
+        if prize_value >= 0:
+            get_number = False
+        else:
+            print("value has to be greater than $0")
+    except ValueError:
+        print("please enter a number")
 name_list = []
 #make list of names and error catching
 while get_list == True:
     get_name = True
     while get_name == True:
-        name = input("please enter name\n")
+        name = input("please enter name for rafle\n")
         if name.isalpha():
             get_name = False
         else:
-            print("Text only")
+            print(f"'{name}' is not a valid name")
     if name.lower() == "end":
         get_list = False
     else:
         name_list.append(name)
-print(name_list)
+length = len(name_list)
+print(f"There are {length} people in the draw for a {prize} valued at {prize_value}.")
+import random
+winner_number = random.randint(0, length)
+winner = name_list[winner_number - 1]
+print(f"And the winner of the {prize}, valued at {prize_value} is {winner}.\ncongratulations {winner}.")
+
