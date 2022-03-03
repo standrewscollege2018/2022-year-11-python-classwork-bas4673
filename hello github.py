9a7e55b30139f6d77c92d0dc9b3d31e9207365a5
bas4673 <bas4673@stacmail.net>
2022-03-04 11:19:08 +1300

    final tweaks
    add comments

--- hello github.py
+++ hello github.py
@@ -1,8 +1,14 @@
-# input for prize in rafle
-get_list = True
-get_prize = True
+''' input for prize in rafle '''
+
+# Import random package so we can generate a winner
+import random
+
+# Set up list to store names
+name_list = []
+
 prize = input("Please enter a prize for the winner of the\n")
-#error catching for prize input
+#error catching for prize
+get_prize = True
 while get_prize == True:
     if prize == "":
         prize = input(f"'{prize}' is not a valid input, please enter a prize\n")
@@ -12,21 +18,26 @@ while get_prize == True:
 get_number = True
 while get_number == True:
     try:
-        prize_value = input(f"\nHow much does the {prize} cost\n")
-        prize_value = int(prize_value)
+        prize_value = int(input(f"\nHow much does the {prize} cost\n"))
+
         if prize_value >= 0:
             get_number = False
         else:
             print("value has to be greater than $0")
     except ValueError:
         print("please enter a number")
-name_list = []
+
 #make list of names and error catching
+get_list = True
 while get_list == True:
+    # Display raffle entrants
     print("Names in raffle")
     print("=" * 35)
     length = len(name_list)
-
+    for i in range(length):
+        print(name_list[i])
+    print("=" * 35)
+    # Get name and error-check for blanks or numbers
     get_name = True
     while get_name == True:
         name = input("\nplease enter name for rafle\n")
@@ -38,10 +49,13 @@ while get_list == True:
         get_list = False
     else:
         name_list.append(name)
+
+# Get number of entrants and print raffle statusus
 length = len(name_list)
-print(f"\nThere are {length} people in the draw for a {prize} valued at {prize_value}.\n")
-import random
+print(f"\nThere are {length} people in the draw for a {prize} valued at ${prize_value}.\n")
+
+# Generate and display winner of raffle
 winner_number = random.randint(0, length)
 winner = name_list[winner_number - 1]
-print(f"And the winner of the {prize}, valued at {prize_value} is {winner}.\ncongratulations {winner}.")
+print(f"And the winner of the {prize}, valued at ${prize_value} is {winner}.\ncongratulations {winner}.")
 
